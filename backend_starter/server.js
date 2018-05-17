06a096283c90fa48ddc40e57400519cab246695f
Gregor <gregor@martynus.net>
2018-05-16 17:47:06 -0700

    starter code

--- backend_starter/server.js
+++ backend_starter/server.js
@@ -1,21 +1,3 @@
-// require('http').createServer((request, response) => {
-//   response.end('Hello, world!')
-// }).listen(8000)
-
-// require express framework
-const express = require('express')
-const randomString = require('random-string')
-
-// initialize the express server
-const app = express()
-
-// Show "Hello, world!" at the root path
-app.get('/', (req, res) => {
-  const uniquId = randomString({length: 7})
-  res.redirect(`/${uniquId}`)
-})
-
-app.use('/:id', express.static('frontend'))
-
-// Start the server, listen to port 3000
-app.listen(3000, () => console.log('App listening on port 3000!'))
+require('http').createServer((request, response) => {
+  response.end('Hello, world!')
+}).listen(8000)
